ae20c3558a67adaed22e541971d8d0f0941f2855
Erwan LE ROUX <erwan.leroux.pro@gmail.com>
2025-12-11 22:11:53 +0100

    🚧 Add API structure

---
+++ WebApi/Controllers/AnnotationController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Abstraction;
+
+namespace WebApi.Controllers;
+
+[Route("api/v1/annotation")]
+public class AnnotationController(ISender sender): ApiController(sender)
+{
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok(new { success = true, message = id });
+    }
+}
---
+++ WebApi/Controllers/EncyclopediaController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Abstraction;
+
+namespace WebApi.Controllers;
+
+[Route("api/v1/encyclopedia")]
+public class EncyclopediaController(ISender sender): ApiController(sender)
+{
+    [HttpGet("filters")]
+    public async Task<IActionResult> Get(CancellationToken cancellationToken)
+    {
+        return Ok();
+    }
+
+    [HttpGet("content-table/{encyclopediaId}")]
+    public async Task<IActionResult> GetContentTable(int encyclopediaId, CancellationToken cancellationToken)
+    {
+        return Ok(new  { success = true, message = encyclopediaId});
+    }
+}
