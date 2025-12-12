4b0427a1cf81a61e5487805cad0c85df78ea30b2
Erwan LE ROUX <erwan.leroux.pro@gmail.com>
2025-12-12 07:25:14 +0100

    🤡 Mock EncyclopediaSearchService for test purposes

---
+++ Application/Services/EncyclopediaSearch/EncyclopediaSearchService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTO;
+using Domain.Shared;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Services.EncyclopediaSearch;
+
+public class EncyclopediaSearchService(ISender sender, ILogger<EncyclopediaSearchService> logger): IEncyclopediaSearchService
+{
+    public async Task<AnswerDto> HandleEncyclopediaSearchAsync(CancellationToken cancellationToken)
+    {
+        EncyclopediaDto result = new EncyclopediaDto(1, "Volume 1", new Guid("6adf4413-ba64-49eb-9cfe-340005f19ca0"));
+
+        return new AnswerDto(true, result, Error.Empty());
+    }
+}
