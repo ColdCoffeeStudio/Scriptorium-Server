dd39ae358ed5cc2d0abbba433146c7989cde29d4
Erwan LE ROUX <erwan.leroux.pro@gmail.com>
2025-12-12 07:03:40 +0100

    🚧 Add API related DTO

---
+++ Application/DTO/ContentTableEntryDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Application.DTO;
+
+public sealed record ContentTableEntryDto(int Id, string ThemeName, NoteIdDto[] notes): IDto;
---
+++ Application/DTO/NoteIdDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Application.DTO;
+
+public sealed record NoteIdDto(int Id, string Title): IDto;
